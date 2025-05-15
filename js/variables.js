ccd28b7eac2701f48211be1bed55256d9e45fc70
satishavarma <satishvarma.dev@gmail.com>
2025-05-15 12:19:13 +0530

    Added comment of reserved keyword with ex

--- js/variables.js
+++ js/variables.js
@@ -1,4 +1,4 @@
-// Naming Variables
+// POINT 1 = Naming Variables
 
 // When we declare a new variable, there are certain rules we need to follow for the name of the variable:
 
@@ -7,3 +7,8 @@
 
 const validVariableName = 120;
 console.log(validVariableName);
+
+
+// POINT 2 =  A reserved keyword cannot be used as a variable name. For example, let is a keyword used to declare variables.We cannot declare a variable with the name l
+
+// const let = 170;  This will show an error console.log(let);
