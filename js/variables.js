b97d505004079ee74c82ab9bdb883f66779aefea
satishavarma <satishvarma.dev@gmail.com>
2025-05-15 12:23:42 +0530

    Added camecase with ex

--- js/variables.js
+++ js/variables.js
@@ -12,3 +12,10 @@ console.log(validVariableName);
 // POINT 2 =  A reserved keyword cannot be used as a variable name. For example, let is a keyword used to declare variables.We cannot declare a variable with the name l
 
 // const let = 170;  This will show an error console.log(let);
+
+
+// POINT 3 Naming Variables - camelCase
+// To distinguish each word, we capitalize the first alphabet of all the words, except for the first word. This is known as camelCase.
+
+const lengthOfBox = 120;
+console.log(lengthOfBox);
