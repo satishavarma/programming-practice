11dd0e2743aa8e8b3db229e27cee65bd0bc2c00b
satishavarma <satishvarma.dev@gmail.com>
2025-05-15 12:11:24 +0530

    Added variables example

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+// Naming Variables
+
+// When we declare a new variable, there are certain rules we need to follow for the name of the variable:
+
+// The name should either start with an alphabet or an underscore
+
+
+const validVariableName = 120;
+console.log(validVariableName);
