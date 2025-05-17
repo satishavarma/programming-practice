0cf6f23ae6aba38cf44171395e0a74e917038662
satishavarma <satishvarma.dev@gmail.com>
2025-05-17 09:20:38 +0530

    Added null data type

--- js/undefinedAndNull.js
+++ js/undefinedAndNull.js
@@ -4,3 +4,9 @@
 
 const name = undefined;
 console.log(name);
+
+
+// POINT 2 = The null Data Type
+// null is a data type in JavaScript. It is used to denote the value of nothing in a variable.
+
+// When a variable is undefined, it means it has been given no value to hold. Whereas, when a variable is null, it means the variable has a value, the value null.
