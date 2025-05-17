b157578c495cef399ec62d367f2b90eaffb9be1b
satishavarma <satishvarma.dev@gmail.com>
2025-05-17 09:02:49 +0530

    Added get the remainder with the example

--- js/number.js
+++ js/number.js
@@ -39,3 +39,9 @@ let numberNegative = 10;
 number--; // This is same as number = number - 1
 
 console.log(numberNegative);
+
+
+// POINT 4 = Get the Remainder
+// We can use % to get the remainder. It returns the remainder after the division of the value on the left of % by the value on the right of %.
+
+console.log(5 % 3); 
