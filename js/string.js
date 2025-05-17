f04a9104220e607973cc6cd32dc95d12cc0c0466
satishavarma <satishvarma.dev@gmail.com>
2025-05-17 08:12:10 +0530

    Created string

---
+++ js/string.js
@@ -0,0 +1,9 @@
+// POINT 1 = Create String
+const fullName = "Sam Smith";
+console.log(fullName);
+
+const book = "The Newest Dragon Stories";
+console.log(book);
+
+const someRandomSentence = "You can be a great programmer!";
+console.log(someRandomSentence);
