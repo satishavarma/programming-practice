ee9e3de8857a2dd656065cd337e2429f500a2f58
satishavarma <satishvarma.dev@gmail.com>
2025-05-17 08:35:10 +0530

    Added negative number example

--- js/number.js
+++ js/number.js
@@ -2,3 +2,10 @@
 // the variable positiveNumber stores the number 1234.
 const positiveNumber = 1234;
 console.log(positiveNumber);
+
+
+// Negative number
+// Numbers can also be negative. In the example given below, the variable negativeNumber stores the number -1234 which is a negative value.
+
+const negativeNumber = -1234;
+console.log(negativeNumber);
