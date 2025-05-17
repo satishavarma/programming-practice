c05140b8e73015480372a5917520cdef632a78ef
satishavarma <satishvarma.dev@gmail.com>
2025-05-17 08:17:02 +0530

    Joining string with plus operator

--- js/string.js
+++ js/string.js
@@ -7,3 +7,13 @@ console.log(book);
 
 const someRandomSentence = "You can be a great programmer!";
 console.log(someRandomSentence);
+
+
+// POINT 2 - Joining Strings
+// We can join strings using the + operator. And we can store the joined result into a new variable of our choice.
+
+const firstName = "Sam";
+const lastName = "Smith";
+const fullNam = firstName + lastName;
+
+console.log(fullNam);
