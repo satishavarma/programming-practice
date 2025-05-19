365f07e94fc65598b868e44a6f943d544e172573
satishavarma <satishvarma.dev@gmail.com>
2025-05-19 22:05:37 +0530

    Added example of using variable as object property

--- js/object.js
+++ js/object.js
@@ -4,3 +4,16 @@ const car = {
   fuelType: "petrol"
 };
 console.log(car);
+
+// POINT 2 = Using Variables as Object Properties
+const noOfSeats = 5;
+
+const carTwo = {
+  model: 2011,
+  seatingCapacity: noOfSeats,
+  fuelType: "petrol"
+};
+
+const result = `It is a ${carTwo.model} model car that runs on ${car.fuelType} and has ${carTwo.seatingCapacity} seats.`
+
+console.log(result);
