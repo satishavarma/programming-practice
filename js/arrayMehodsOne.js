09b35494697ba723389fe2397874ed666a70863c
satishavarma <satishvarma.dev@gmail.com>
2025-05-20 21:59:15 +0530

    Added unshift method with example

--- js/arrayMehodsOne.js
+++ js/arrayMehodsOne.js
@@ -5,3 +5,12 @@ vegetables.push("beetroot");
 
 console.log(vegetables);
 // vegetables.push("beetroot") adds a new element "beetroot" to the end of the vegetables array.
+
+// Method 2 = Adding Array Element - unshift
+// The unshift() method is used to add a new element at the beginning of an array.
+// vegetables.unshift("beetroot") adds a new element "beetroot" to the beginning of the array vegetables.
+
+const vegis = ["corn", "carrot", "potato"];
+vegis.unshift("beetroot");
+
+console.log(vegis);
