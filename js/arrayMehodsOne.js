0c841101aff618195aea6934d745e30c4852c3f5
satishavarma <satishvarma.dev@gmail.com>
2025-05-20 21:55:39 +0530

    Added push method with example

---
+++ js/arrayMehodsOne.js
@@ -0,0 +1,7 @@
+// POINT 1 = Adding Array Element - push
+// The push() method is used to add a new element to the end of an array.
+const vegetables = ["corn", "carrot", "potato"];
+vegetables.push("beetroot");
+
+console.log(vegetables);
+// vegetables.push("beetroot") adds a new element "beetroot" to the end of the vegetables array.
