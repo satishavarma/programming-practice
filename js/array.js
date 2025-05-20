2a065ed878cfd330298ad97502bcc4a0b5f39293
satishavarma <satishvarma.dev@gmail.com>
2025-05-20 19:16:45 +0530

    Creating an array

---
+++ js/array.js
@@ -0,0 +1,6 @@
+// POINT 1 = Create Array
+// Arrays in JavaScript are special kind of objects. Similar to an object, it can hold a mix of different data types.
+
+const mixedArray = [1, "hello", true, null, undefined];
+
+console.log(mixedArray);
