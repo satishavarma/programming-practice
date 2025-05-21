ea92107fcc0fd9841457b86815f892ae1b881b66
satishavarma <satishvarma.dev@gmail.com>
2025-05-21 22:37:11 +0530

    Added example of falsy values

--- js/conditionals.js
+++ js/conditionals.js
@@ -9,3 +9,11 @@ console.log(Boolean(age));
 
 const experience = 0;
 console.log(Boolean(experience));
+
+// IMPORTANT NOTE = Values such as the number 0, false, null, undefined, NaN, and the empty string "", will get converted to false. These values are called falsy values.
+
+console.log(Boolean(0));
+console.log(Boolean(false));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
