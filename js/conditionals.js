4eab9fd460a7fa406ee15a777ea61c7953ce7a02
satishavarma <satishvarma.dev@gmail.com>
2025-05-21 22:35:37 +0530

    Added example of converted value to boolean

---
+++ js/conditionals.js
@@ -0,0 +1,11 @@
+// POINT 1 = Truthy and Falsy Values
+// In JavaScript, we can convert values of any data type to boolean. We can use Boolean() to convert any value to a boolean value.
+
+const name = "Sam Smith";
+console.log(Boolean(name));
+
+const age = 32;
+console.log(Boolean(age));
+
+const experience = 0;
+console.log(Boolean(experience));
