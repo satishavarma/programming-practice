31418281702428cfb7f79ce63d6d6a000bc49537
satishavarma <satishvarma.dev@gmail.com>
2025-06-01 01:09:36 +0530

    Added and logical expression with example

--- js/logicalOperation.js
+++ js/logicalOperation.js
@@ -8,3 +8,21 @@ if (guest.role === "faculty" || guest.role === "volunteer") {
 } else {
   console.log(`Access denied`);
 }
+
+
+
+// AND
+
+const courseFee = 10000;
+const courseDiscountAmount = 3000;
+const student = { 
+  marks: 78,
+  internshipDone: true
+};
+
+if (student.marks >= 75 && student.internshipDone) {
+  const discountedFee = courseFee - courseDiscountAmount;
+  console.log(`Your course fee after discount is: ${discountedFee}.`);
+} else {
+  console.log(`Your course fee is: ${courseFee}.`);
+}
