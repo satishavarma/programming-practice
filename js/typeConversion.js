07423812d658a826d77f3491c142f218b94c6d7a
satishavarma <satishvarma.dev@gmail.com>
2025-06-03 08:03:01 +0530

    Added an example of number to string

---
+++ js/typeConversion.js
@@ -0,0 +1,7 @@
+// Converting to String
+// Sometimes we need to explicitly convert a value to string. In such cases, we can use either String() or toString() to convert a non-string value to a string.
+
+// Number to String
+console.log(2);
+console.log(String(2));
+console.log((2).toString());
